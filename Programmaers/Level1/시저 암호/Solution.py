d23ebc226e9621150e1a82ffbed57a4f62867d2b
KimJinHye0n <63627253+KimJinHye0n@users.noreply.github.com>
2020-08-21 23:55:52 +0900

    0816 level1

--- Programmaers/Level1/시저 암호/Solution.py
+++ Programmaers/Level1/시저 암호/Solution.py
@@ -1,6 +1,19 @@
-def solution(phone_number):
-    x = phone_number[-4:]
-    y = len(str(phone_number)) - len(str(x))
-    z = y*'*'
-    answer = z + x
+# 아스키코드 : A - 65 / Z - 90 / a - 97 / z - 122 / 공백 - 32
+# chr : 아스키코드 -> 문자 / ord : 문자 -> 아스키코드
+def solution(s, n):
+    answer = ''
+    for i in s :
+        if ord(i) == 32 :
+            answer+=i
+        else :
+            if ord(i) >= 65 and ord(i) <= 90 :
+                ordn = ord(i) + n
+                if ordn > 90 :
+                    ordn = ordn - 91 + 65
+                answer+=chr(ordn)
+            elif ord(i) >= 96 and ord(i) <=122 :
+                ordn = ord(i) + n
+                if ordn > 122 :
+                    ordn = ordn - 123 + 97
+                answer+=chr(ordn)
     return answer
